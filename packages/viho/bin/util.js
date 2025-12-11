e6c9df257d868609987c59790dcc4c3898d2e750
uikoo9 <uikoo9@qq.com>
2025-12-11 19:37:52 +0800

    feat(viho): 抽取ask方法

--- packages/viho/bin/util.js
+++ packages/viho/bin/util.js
@@ -4,6 +4,9 @@ const os = require('os');
 // path
 const path = require('path');
 
+// qiao
+const cli = require('qiao-cli');
+
 // db
 const DB = require('qiao-config');
 
@@ -14,4 +17,70 @@ const DB = require('qiao-config');
 exports.getDB = () => {
   const dbPath = path.resolve(os.homedir(), './viho.json');
   return DB(dbPath);
+};
+
+/**
+ * ask
+ * @param {*} llm
+ * @param {*} model
+ */
+exports.ask = async (llm, model) => {
+  // ask
+  const questions = [
+    {
+      type: 'editor',
+      name: 'content',
+      message: 'Your question:',
+    },
+  ];
+  const answers = await cli.ask(questions);
+
+  // answers
+  console.log();
+  console.log(cli.colors.gray('Question:'));
+  console.log(cli.colors.gray(answers.content));
+  console.log();
+
+  // chat
+  const chatOptions = {
+    model: model.modelID,
+    messages: [
+      { role: 'system', content: 'You are a helpful AI assistant' },
+      { role: 'user', content: answers.content },
+    ],
+    thinking: {
+      type: model.modelThinking,
+    },
+  };
+
+  // callback options
+  const callbackOptions = {
+    firstThinkingCallback: () => {
+      console.log();
+      console.log(cli.colors.gray('[Thinking...]'));
+      console.log();
+    },
+    thinkingCallback: (msg) => {
+      process.stdout.write(cli.colors.gray(msg));
+    },
+    firstContentCallback: () => {
+      console.log();
+      console.log(cli.colors.cyan('[Response]'));
+      console.log();
+    },
+    contentCallback: (msg) => {
+      process.stdout.write(msg);
+    },
+    endCallback: () => {
+      console.log();
+    },
+    errorCallback: (error) => {
+      console.log();
+      console.log(cli.colors.red('Error:'));
+      console.log(error);
+    },
+  };
+
+  // go
+  await llm.chatWithStreaming(chatOptions, callbackOptions);
 };
--- packages/viho/bin/viho-ask.js
+++ packages/viho/bin/viho-ask.js
@@ -8,6 +8,9 @@ const LLM = require('qiao-llm');
 const { getDB } = require('./util.js');
 const db = getDB();
 
+// util
+const { ask } = require('./util.js');
+
 // cmd
 cli.cmd
   .command('ask [modelName]')
@@ -36,62 +39,5 @@ cli.cmd
       baseURL: model.baseURL,
     });
 
-    // ask
-    const questions = [
-      {
-        type: 'editor',
-        name: 'content',
-        message: 'Your question:',
-      },
-    ];
-    const answers = await cli.ask(questions);
-
-    // answers
-    console.log();
-    console.log(cli.colors.gray('Question:'));
-    console.log(cli.colors.gray(answers.content));
-    console.log();
-
-    // chat
-    const chatOptions = {
-      model: model.modelID,
-      messages: [
-        { role: 'system', content: 'You are a helpful AI assistant' },
-        { role: 'user', content: answers.content },
-      ],
-      thinking: {
-        type: model.modelThinking,
-      },
-    };
-
-    // callback options
-    const callbackOptions = {
-      firstThinkingCallback: () => {
-        console.log();
-        console.log(cli.colors.gray('[Thinking...]'));
-        console.log();
-      },
-      thinkingCallback: (msg) => {
-        process.stdout.write(cli.colors.gray(msg));
-      },
-      firstContentCallback: () => {
-        console.log();
-        console.log(cli.colors.cyan('[Response]'));
-        console.log();
-      },
-      contentCallback: (msg) => {
-        process.stdout.write(msg);
-      },
-      endCallback: () => {
-        console.log();
-      },
-      errorCallback: (error) => {
-        console.log();
-        console.log(cli.colors.red('Error:'));
-        console.log(error);
-      },
-    };
-
-    // go
-    await llm.chatWithStreaming(chatOptions, callbackOptions);
+    await ask(llm, model);
   });
