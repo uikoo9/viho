cdf3bb8e55389ccb7f1c297537e89f2525b1215a
uikoo9 <uikoo9@qq.com>
2025-12-12 17:11:37 +0800

    feat(viho): viho print logo

--- packages/viho/bin/viho.js
+++ packages/viho/bin/viho.js
@@ -3,11 +3,19 @@
 // qiao
 const cli = require('qiao-cli');
 
+// util
+const { printLogo } = require('../src/util.js');
+
 // cmds
 require('./viho-model.js');
 require('./viho-ask.js');
 require('./viho-chat.js');
 require('./viho-version.js');
+
+// print logo if no args
+if (process.argv.length === 2) {
+  printLogo();
+}
 
 // parse
 cli.cmd.parse(process.argv);
