1434f29cd89aeac326509ee3296e6fa6f201799f
uikoo9 <uikoo9@qq.com>
2026-03-17 09:15:32 +0800

    feat(viho): break agent

--- packages/viho-llm/src/agents/agent.js
+++ packages/viho-llm/src/agents/agent.js
@@ -13,10 +13,17 @@ export const runAgents = async (agents) => {
     const agentStartCallback = agent.agentStartCallback;
     const agentRequestOptions = agent.agentRequestOptions;
     const agentEndCallback = agent.agentEndCallback;
+    const agentBreakCallback = agent.agentBreakCallback;
 
     // go
     agentStartCallback();
     const agentResponse = await callLLM(agentRequestOptions);
-    agentEndCallback(agentResponse);
+
+    // check
+    const breakAgent = agentEndCallback(agentResponse);
+    if (breakAgent) {
+      if (agentBreakCallback) agentBreakCallback();
+      break;
+    }
   }
 };
